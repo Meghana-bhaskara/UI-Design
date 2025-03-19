6e76fdde237e6e842b4c13845988c03e90351f16
Meghana-bhaskara <meghana.bhaskaraa@gmail.com>
2025-03-19 20:00:59 +0530

    Update switch3.dart

--- LAB4/project-switch/lib/switch3.dart
+++ LAB4/project-switch/lib/switch3.dart
@@ -1,14 +1,19 @@
 class SwitchLabel{
-void sdemo(String fruit){
-     switch(fruit){
-          
+void sdemo(String month){
+     switch(month){
+          first : case 'jan':
+                  print('31 days');
+                  break;
+                  case 'feb':
+                  print('28 days');
+                  break;
+                  case 'march':
+                  print('31 days');
+                  break;
+                  default:
+                    print("enter a valid month");
+          continue first;
 
-         
-
-
-
-
-  
      }
 }
 }
